3e8e73b433dbcd7d6b4434e7f6afdd3647bd173c
ButcherT360 <ik4ros@hotmail.fi>
2026-05-12 14:34:39 +0300

    varauslista

--- calendar-ui/src/App.js
+++ calendar-ui/src/App.js
@@ -7,6 +7,8 @@ import "./App.css";
 function App() {
   const [events, setEvents] = useState([]);
 
+  const [loading, setLoading] = useState(false);
+
   const [showModal, setShowModal] = useState(false);
   const [selectedDate, setSelectedDate] = useState("");
   const [name, setName] = useState("");
@@ -16,7 +18,12 @@ function App() {
   const [deleteInput, setDeleteInput] = useState("");
 
   // hae varaukset
+
+
+  
   const loadBookings = async () => {
+    //setLoading(true);
+    //await new Promise(r => setTimeout(r, 1000)); // pakottaa 2s viive
     try {
       const res = await fetch("http://localhost:3000/bookings");
       const data = await res.json();
@@ -34,7 +41,9 @@ function App() {
       );
     } catch (err) {
       console.log("LOAD ERROR:", err);
-    }
+    } finally {
+    setLoading(false);
+  }
   };
 
   useEffect(() => {
@@ -45,8 +54,8 @@ function App() {
   function handleDateClick(info) {
     const date = info.dateStr;
 
-    const exists = events.some(e => e.start === date);
-    if (exists) {
+    const isBooked = events.some(e => e.start === date);
+    if (isBooked) {
       alert("Tämä päivä on jo varattu!");
       return;
     }
@@ -128,21 +137,81 @@ function App() {
   }
 
   return (
-    <div style={{ padding: 20 }}>
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        events={events}
-        dateClick={handleDateClick}
-        eventClick={handleEventClick}
-        dayCellClassNames={(arg) => {
-          const date = arg.date.toLocaleDateString("sv-SE");
-          const booked = events.some(e => e.start === date);
+  <div style={{ padding: 20 }}>
 
-          return booked ? "booked" : "free";
-        }}
-      />
+    {loading && <p>Ladataan varauksia...</p>}
 
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      dateClick={handleDateClick}
+      eventClick={handleEventClick}
+
+      dayCellClassNames={(arg) => {
+        const year = arg.date.getFullYear();
+        const month = String(arg.date.getMonth() + 1).padStart(2, "0");
+        const day = String(arg.date.getDate()).padStart(2, "0");
+
+        const date = `${year}-${month}-${day}`;
+
+        const isBooked = events.some(e => e.start === date);
+
+        return isBooked ? "booked" : "free" ;
+        <div> 
+          <div>{arg.dayNumberText}</div>
+
+          {isBooked && (
+            <div style={{ fontSize: "14px", color: "red" }}>
+              VARATTU
+            </div>
+          )}
+        </div>
+
+        
+      }}
+      
+      dayCellContent={(arg) => {
+        const year = arg.date.getFullYear();
+        const month = String(arg.date.getMonth() + 1).padStart(2, "0");
+        const day = String(arg.date.getDate()).padStart(2, "0");
+
+        const date = `${year}-${month}-${day}`;
+        const booked = events.some(e => e.start === date);
+        const isBooked = events.some(e => e.start === date);
+
+        return (
+          <div>
+            <div>{arg.dayNumberText}</div>
+
+            {isBooked && (
+              <div style={{ fontSize: "14px", color: "red" }}>
+                VARATTU
+              </div>
+            )}
+          </div>
+        );
+      }}
+      
+    />
+    <div>
+    <h2>Varaukset</h2>
+
+    {events.length === 0 ? (
+      <p>Ei varauksia</p>
+    ) : (
+      events.map(event => (
+        <div key={event.id} className="bookingItem">
+          <strong>{event.start}</strong> – {event.title}
+
+          <button onClick={() => setDeleteTarget(event)}>
+            Poista
+          </button>
+        </div>
+      ))
+    )}</div>
+
+      
       {/* CREATE MODAL */}
       {showModal && (
         <div className="modalOverlay">
