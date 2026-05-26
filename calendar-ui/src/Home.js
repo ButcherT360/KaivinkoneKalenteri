a28c12597322d71b81a9963829423ef53cc89dd9
ButcherT360 <ik4ros@hotmail.fi>
2026-05-26 15:17:30 +0300

    frontend

--- calendar-ui/src/Home.js
+++ calendar-ui/src/Home.js
@@ -2,10 +2,7 @@ export default function Home() {
     return (
             <div className="layout">
                 <h1 className="title-bar">Etusivu</h1>
-
-                <div className="container">
-                    <img src="/logo.png" alt="kaivurilogo" />
-                </div>
+                <img src="/logo.png" alt="kaivurilogo" />
             </div>
     );
 }
