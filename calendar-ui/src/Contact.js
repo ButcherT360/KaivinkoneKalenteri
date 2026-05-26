8c073f23ad65bc17b52a30009ca3f9513ca6e4d2
ButcherT360 <ik4ros@hotmail.fi>
2026-05-26 13:48:19 +0300

    frontend

--- calendar-ui/src/Contact.js
+++ calendar-ui/src/Contact.js
@@ -2,19 +2,8 @@ export default function Contact() {
     return (
         <div style={{ padding: 20 }}>
             <h1 className="title-bar">Yhteystiedot</h1>
-
-            <div>
-                <img
-                    src="/logo.png"
-                    alt="Logo"
-                    style={{
-                        width: "100%",
-                        maxWidth: "720px",
-                        height: "100%",
-                        maxHeight: "1080px",
-                        borderRadius: "10px"
-                    }}
-                />
+            <div className="container">
+                <img src="/logo.png" />
             </div>
         </div>
 
--- calendar-ui/src/Home.js
+++ calendar-ui/src/Home.js
@@ -3,18 +3,8 @@ export default function Home() {
         <div style={{ padding: 20 }}>
             <h1 className="title-bar">Etusivu</h1>
 
-            <div>
-                <img
-                    src="/logo.png"
-                    alt="Logo"
-                    style={{
-                        width: "100%",
-                        maxWidth: "720px",
-                        height: "100%",
-                        maxHeight: "1080px",
-                        borderRadius: "10px"
-                    }}
-                />
+            <div className="container">
+                <img src="/logo.png" />
             </div>
         </div>
     );
