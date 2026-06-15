428d9ee55274b807cdc4b1df67b0337abff1c3d9
ButcherT360 <ik4ros@hotmail.fi>
2026-06-15 10:16:06 +0300

    frontend

--- calendar-ui/src/Machines.js
+++ calendar-ui/src/Machines.js
@@ -1,43 +1,49 @@
 import { useState } from "react";
 
 export default function Machines() {
-  const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
 
-  const images = [
-    "/logo.png",
-    "/Kauha_1.jpeg",
-    "/Kauha_2.jpeg",
-    "/Kauha_3.jpeg",
-    "/Kauhat_1_2.jpeg"
-  ];
+    const images = [
+        "/logo.png",
+        "/Kauha_1.jpeg",
+        "/Kauha_2.jpeg",
+        "/Kauha_3.jpeg",
+        "/Kauhat_1_2.jpeg"
+    ];
 
-  return (
-    <div>
-      <h1 className="title-bar">Vuokrattava kone ja kauhat</h1>
+    return (
+        <div>
+            <h1 className="title-bar">Vuokrattava kone ja kauhat</h1>
 
-      <div className="gallery">
-        {images.map((img, i) => (
-          <img
-            key={i}
-            src={img}
-            alt=""
-            onClick={() => setSelectedImage(img)}
-          />
-        ))}
-      </div>
+            <div className="gallery">
+                {images.map((img, i) => (
+                    <img
+                        key={i}
+                        src={img}
+                        alt=""
+                        onClick={() => setSelectedImage(img)}
+                    />
+                ))}
+            </div>
 
-      {selectedImage && (
-        <div
-          className="lightbox"
-          onClick={() => setSelectedImage(null)}
-        >
-          <img
-            src={selectedImage}
-            alt=""
-            onClick={(e) => e.stopPropagation()}
-          />
+            {selectedImage && (
+                <div
+                    className="lightbox"
+                    onClick={() => setSelectedImage(null)}
+                >
+                    <img
+                        src={selectedImage}
+                        alt=""
+                        onClick={(e) => e.stopPropagation()}
+                    />
+                </div>
+            )}console.log("CLICK", img);
+
+            onClick={() => {
+                console.log("CLICK", img);
+                setSelectedImage(img);
+            }}
         </div>
-      )}
-    </div>
-  );
+
+    );
 }
