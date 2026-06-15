17581c3fd4999d1a2149407a630ec89d0b66a05b
ButcherT360 <ik4ros@hotmail.fi>
2026-06-15 10:00:46 +0300

    frontend

--- calendar-ui/src/Machines.js
+++ calendar-ui/src/Machines.js
@@ -1,20 +1,43 @@
+import { useState } from "react";
+
 export default function Machines() {
-    const images = [
-        "logo.png",
-        "/Kauha_1.jpeg",
-        "/Kauha_2.jpeg",
-        "/Kauha_3.jpeg",
-        "/Kauhat_1_2.jpeg"
-    ];
+  const [selectedImage, setSelectedImage] = useState(null);
 
-    return (
-        <div><h1 className="title-bar">Vuokrattava kone ja kauhat</h1>
+  const images = [
+    "/logo.png",
+    "/Kauha_1.jpeg",
+    "/Kauha_2.jpeg",
+    "/Kauha_3.jpeg",
+    "/Kauhat_1_2.jpeg"
+  ];
 
-            <div className="gallery">
-                {images.map((img, i) => (
-                    <img key={i} src={img} alt="" />
-                ))}
-            </div>
+  return (
+    <div>
+      <h1 className="title-bar">Vuokrattava kone ja kauhat</h1>
+
+      <div className="gallery">
+        {images.map((img, i) => (
+          <img
+            key={i}
+            src={img}
+            alt=""
+            onClick={() => setSelectedImage(img)}
+          />
+        ))}
+      </div>
+
+      {selectedImage && (
+        <div
+          className="lightbox"
+          onClick={() => setSelectedImage(null)}
+        >
+          <img
+            src={selectedImage}
+            alt=""
+            onClick={(e) => e.stopPropagation()}
+          />
         </div>
-    );
+      )}
+    </div>
+  );
 }
