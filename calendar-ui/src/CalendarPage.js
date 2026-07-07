37ccbf3a62b5098e1a64849e4ce08c778ff439a4
ButcherT360 <ik4ros@hotmail.fi>
2026-07-07 10:25:36 +0300

    footer

--- calendar-ui/src/CalendarPage.js
+++ calendar-ui/src/CalendarPage.js
@@ -1,5 +1,4 @@
 import { useEffect, useState, useMemo } from "react";
-import { Link } from "react-router-dom";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
