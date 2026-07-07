657b71da9ea966b2ca018256be83c3acb76f6d50
ButcherT360 <ik4ros@hotmail.fi>
2026-07-07 10:34:53 +0300

    footer

--- calendar-ui/src/Footer.js
+++ calendar-ui/src/Footer.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>© 2026 Kaivinkonevuokraus</p>
-            {/*<p>Puhelin: </p>
-      <p>Sähköposti: </p>*/}
             <div className="contact-center">
                 <Link to="/yhteystiedot" className="contact-btn">
                     Ota yhteyttä
                 </Link>
             </div>
+            <p>© 2026 Kaivinkonevuokraus</p>
+            {/*<p>Puhelin: </p>
+      <p>Sähköposti: </p>*/}
         </footer>
     );
 }
